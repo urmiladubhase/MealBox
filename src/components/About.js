aa0360fe0a64a378eaa1b468fe3b81805ad349ce
Urmila Dubhase <urmi@Urmilas-MacBook-Air.local>
2025-08-06 15:18:56 +0530

    adding the user

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
+import User from "./User";
+import UserClass from "./UserClass";
 export const About = () => {
     return (
-        <div className="about">
+       <div className="about">
             <h1>About Us</h1>
-            <p>We are a team of passionate developers dedicated to creating amazing web applications.</p>
-            <p>Our mission is to deliver high-quality software solutions that meet the needs of our users.</p>
             <p>Thank you for visiting our website!</p>
+            <User  name = {"Urmi "} location={"India"} contact={"123-456-7890"}/>
+            <UserClass name = {"Urmi "} location={"India"} />
         </div>
     );
-}
+};
+
+export default About;
